90e3a8ef0f810e1e47968cf6c0db489be12fcaa8
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-04-04 17:52:56 +0800

    able to connect, list instances and run new instance

--- AwsClient/src/main/java/prompto/aws/EC2.java
+++ AwsClient/src/main/java/prompto/aws/EC2.java
@@ -1,10 +1,37 @@
 package prompto.aws;
 
+import java.util.Base64;
+
+
+
+
+
+
+
+
+import prompto.intrinsic.PromptoConverter;
+import prompto.intrinsic.PromptoDocument;
+import prompto.intrinsic.PromptoList;
+
+
+
+
+
+
+
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;import com.amazonaws.services.ec2.model.Tag;
+import com.fasterxml.jackson.databind.JsonNode;import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 public class EC2 {
 	
@@ -22,6 +49,47 @@ public class EC2 {
 
 	public EC2(AmazonEC2 ec2) {
 		this.ec2 = ec2;
+	}
+	
+	public String runInstance(String imageId, String instanceType, String keyName, PromptoList<String> securityGroups, String userData) {
+		userData = Base64.getEncoder().encodeToString(userData.getBytes());
+		RunInstancesRequest runRequest = new RunInstancesRequest()
+			.withImageId(imageId)
+			.withInstanceType(instanceType)
+			.withMinCount(1)
+            .withMaxCount(1)
+            .withKeyName(keyName)
+            .withSecurityGroups(securityGroups)
+            .withUserData(userData);
+		RunInstancesResult runResult = ec2.runInstances(runRequest);
+		return runResult.getReservation().getInstances().get(0).getInstanceId();
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public PromptoList<PromptoDocument<String, Object>> listInstances() {
+		PromptoList<PromptoDocument<String, Object>> list = new PromptoList<PromptoDocument<String,Object>>(true);
+		DescribeInstancesResult result = ec2.describeInstances();
+		result.getReservations().forEach((r)->{
+			r.getInstances().forEach((i)->{
+				JsonNode json = new ObjectMapper().valueToTree(i);
+				Object prompto = PromptoConverter.nodeToPrompto(json);
+				assert (prompto instanceof PromptoDocument);
+				PromptoDocument<String, Object> doc = (PromptoDocument<String, Object>)prompto;
+				promoteNameTag(i, doc);
+				list.add(doc); 
+			});
+		});
+		return list;
+	}
+
+	private void promoteNameTag(Instance instance, PromptoDocument<String, Object> doc) {
+		Tag tag = instance.getTags().stream()
+				.filter((t)->"Name".equals(t.getKey()))
+				.findFirst()
+				.orElse(null);
+		if(tag!=null)
+			doc.put("Name", tag.getValue());
 	}
 
 }
--- AwsClient/src/test/java/prompto/aws/TestDescribeInstances.java
+++ AwsClient/src/test/java/prompto/aws/TestDescribeInstances.java
@@ -1,9 +1,15 @@
 package prompto.aws;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Tag;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Category(AwsTest.class)
 public class TestDescribeInstances extends EC2TestBase {
@@ -12,6 +18,14 @@ public class TestDescribeInstances extends EC2TestBase {
 	public void testThatDescribeInstancesReturnsData() throws Exception {
 		DescribeInstancesResult result = ec2.describeInstances();
 		System.out.println(result.toString());
+	}
+	
+	@Test
+	public void testInstanceTag() throws Exception {
+		Instance instance = new Instance()
+			.withTags(new Tag().withKey("k").withValue("v"));
+		JsonNode node = new ObjectMapper().valueToTree(instance);
+		assertNotNull(node.get("tags"));
 	}
 
 }
