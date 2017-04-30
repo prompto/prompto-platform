acf478d9b940ffbafd192861dc95a9544b17b045
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-04-30 10:29:33 +0800

    fix class path issues

--- DataServer/src/test/java/prompto/dataserver/Application.java
+++ DataServer/src/test/java/prompto/dataserver/Application.java
@@ -11,12 +11,18 @@ public class Application {
 	public static void main(String[] args) throws Throwable {
 		List<String> argsList = new ArrayList<>(Arrays.asList(args));
 		argsList.add("-resources");
-		argsList.add("DataExplorer.pec");
+		argsList.add(getResourcesList());
 		argsList.add("-application");
 		argsList.add("data-explorer");
 		argsList.add("-version");
 		argsList.add("1.0.0");
+		argsList.add("-web-site");
+		argsList.add("../data-explorer/web-site/");
 		AppServer.main(argsList.toArray(new String[argsList.size()]));
+	}
+
+	private static String getResourcesList() {
+		return "\"" + Thread.currentThread().getContextClassLoader().getResource("libraries/DataExplorer.pec").toExternalForm() + "\"";
 	}
 
 }
