9b73de946dbd0b7c66790eb3f15f8329235b7d3f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-09-17 17:26:44 +0800

    implement LoginModule which retrieves password digest from store

---
+++ Server/src/main/java/prompto/security/StoredHashedPasswordLoginModule.java
@@ -0,0 +1,115 @@
+package prompto.security;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+
+import org.apache.xerces.impl.dv.util.Base64;
+import org.eclipse.jetty.jaas.spi.UserInfo;
+import org.eclipse.jetty.util.security.Credential;
+
+import prompto.config.ILoginConfiguration;
+import prompto.config.IStoreConfiguration;
+import prompto.store.AttributeInfo;
+import prompto.store.Family;
+import prompto.store.IQueryBuilder;
+import prompto.store.IQueryBuilder.MatchOp;
+import prompto.store.IStore;
+import prompto.store.IStoreFactory;
+import prompto.store.IStored;
+
+public class StoredHashedPasswordLoginModule extends LoginModuleBase {
+
+	IStore store;
+	
+	static final AttributeInfo LOGIN = new AttributeInfo("login", Family.TEXT, false, Arrays.asList("key"));
+	static final AttributeInfo METHOD = new AttributeInfo("method", Family.TEXT, false, null);
+	static final AttributeInfo DIGEST = new AttributeInfo("digest", Family.TEXT, false, null);
+	static final AttributeInfo QUESTIONS = new AttributeInfo("questions", Family.CATEGORY, true, null);
+	static final AttributeInfo QUESTION = new AttributeInfo("question", Family.TEXT, false, null);
+	static final AttributeInfo ANSWER = new AttributeInfo("answer", Family.TEXT, false, null);
+	
+	@Override
+	public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
+		super.initialize(subject, callbackHandler, sharedState, options);
+		ILoginConfiguration config = (ILoginConfiguration)options.get("config");
+		IStoreConfiguration storeConfig = config.getStoreConfiguration();
+		try {
+			store = IStoreFactory.newStoreFromConfig(storeConfig);
+			store.createOrUpdateColumns(Arrays.asList(LOGIN, DIGEST, QUESTIONS, QUESTION, ANSWER));
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	@Override
+	public UserInfo getUserInfo(String username) throws Exception {
+		return new UserInfo(username, new PasswordDigestIsStoredCredential(username), Collections.singletonList("*"));
+	}
+	
+	
+	static final Map<String, Function<Object, String>> methods = Collections.singletonMap("MD5", StoredHashedPasswordLoginModule::digestMD5);
+	
+	public static String digestMD5(Object credentials) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(credentials.toString().getBytes());
+			return Base64.encode(md.digest());
+		} catch(Throwable t) {
+			t.printStackTrace();
+			return null;
+		}
+	}
+
+	@SuppressWarnings("serial")
+	class PasswordDigestIsStoredCredential extends Credential {
+
+		String username;
+		
+		public PasswordDigestIsStoredCredential(String username) {
+			this.username = username;
+		}
+
+		@Override
+		public boolean check(Object credentials) {
+			IQueryBuilder query = store.newQueryBuilder();
+			query.verify(LOGIN, MatchOp.EQUALS, this.username);
+			IStored stored = store.fetchOne(query.build());
+			if(stored==null)
+				return isNullCredentials(credentials);
+			Function<Object, String> method = methods.get(stored.getData(METHOD.getName()));
+			if(method==null)
+				return false;
+			else {
+				Object value = stored.getData(DIGEST.getName());
+				return checkEqual(value, credentials, method);
+			}
+		}
+		
+		boolean checkEqual(Object stored, Object proposed, Function<Object, String> method) {
+			if(stored==null) 
+				return isNullCredentials(proposed);
+			else if(isNullCredentials(proposed))
+				return false;
+			String digest = method.apply(proposed);
+			return stored.equals(digest);
+		}
+
+		private boolean isNullCredentials(Object credentials) {
+			return (credentials==null || (credentials instanceof String && credentials.toString().isEmpty()));
+		}
+
+		
+	}
+	
+	
+
+
+	
+}
---
+++ Server/src/test/java/prompto/server/TestLoginModule.java
@@ -0,0 +1,124 @@
+package prompto.server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+
+import org.apache.xerces.impl.dv.util.Base64;
+import org.junit.Before;
+import org.junit.Test;
+
+import prompto.config.IHttpConfiguration;
+import prompto.config.IKeyStoreConfiguration;
+import prompto.config.ILoginConfiguration;
+import prompto.config.IStoreConfiguration;
+import prompto.memstore.MemStore;
+import prompto.security.StoredHashedPasswordLoginModule;
+import prompto.store.IStorable;
+import prompto.store.IStore;
+import prompto.store.IStoreFactory;
+
+public class TestLoginModule extends BaseServerTest {
+
+	static IStore store = new MemStore(); // need a static to share MemStore across classes
+
+	public static class StoreFactoryTest implements IStoreFactory {
+
+		@Override
+		public IStore newStore(IStoreConfiguration config) throws Exception {
+			return store;
+		}
+		
+	}
+	
+	@Override
+	protected IHttpConfiguration getHttpConfiguration() {
+		return new IHttpConfiguration() {
+			@Override public String getProtocol() { return "http"; }
+			@Override public int getPort() { return port; }
+			@Override public String getAllowedOrigin() { return null; }
+			@Override public IKeyStoreConfiguration getKeyStoreConfiguration() { return null; }
+			@Override public IKeyStoreConfiguration getTrustStoreConfiguration() { return null; }
+			@Override public ILoginConfiguration getLoginConfiguration() { 
+				return new ILoginConfiguration() {
+					@Override
+					public String getModuleName() {
+						return StoredHashedPasswordLoginModule.class.getName();
+					}
+					@Override public IStoreConfiguration getStoreConfiguration() { 
+						return new IStoreConfiguration() {
+							@Override public IStoreConfiguration withDbName(String dbName) { return null; }
+							@Override public String getUser() { return null; }
+							@Override public Integer getPort() { return null; }
+							@Override public char[] getPassword() { return null; }
+							@Override public String getHost() { return null; }
+							@Override
+							public String getFactory() {
+								return StoreFactoryTest.class.getName();
+							}
+							@Override public String getDbName() { return null; }
+						}; 
+					}
+				}; 
+			}
+		};
+	}
+	
+	@Before
+	public void before() {
+		store.deleteAll();
+	}
+
+	@Test
+	public void testThatUnknownUserIsRejected() throws Exception {
+		int code = loadResource("john", "password");
+		assertEquals(401, code);
+	}
+
+	@Test
+	public void testThatKnownUserIsAllowedWhenUsingCorrectPassword() throws Exception {
+		createUserHashingPasswordWithMD5("john", "password");
+		int code = loadResource("john", "password");
+		assertEquals(200, code);
+	}
+
+	@Test
+	public void testThatKnownUserIsRejectedWhenUsingIncorrectPassword() throws Exception {
+		createUserHashingPasswordWithMD5("john", "password");
+		int code = loadResource("john", "wrong");
+		assertEquals(401, code);
+	}
+
+	private void createUserHashingPasswordWithMD5(String login, String password) {
+		IStorable storable = store.newStorable(Arrays.asList("User"), null);
+		storable.setData("login", "john");
+		storable.setData("method", "MD5");
+		storable.setData("digest", StoredHashedPasswordLoginModule.digestMD5(password));
+		store.store(storable);
+	}
+
+	static final String HTTP_CODE_PREFIX = "Server returned HTTP response code: ";
+	
+	private int loadResource(String login, String password) throws Exception {
+		URL url = new URL("http://localhost:" + port + "/js/lib/require.js");
+		URLConnection cnx = url.openConnection();
+		String authorization = Base64.encode((login + ":" + password).getBytes());
+		cnx.setRequestProperty("Authorization", "Basic " + authorization);		
+		try (InputStream input = cnx.getInputStream()) {
+			return 200;
+		} catch(IOException e) {
+			String msg = e.getMessage();
+			if(msg.startsWith(HTTP_CODE_PREFIX)) {
+				msg = msg.substring(HTTP_CODE_PREFIX.length()) ;
+				return Integer.parseInt(msg.substring(0, 3));
+			} else
+				throw e;
+		}
+	}
+}
+
+
