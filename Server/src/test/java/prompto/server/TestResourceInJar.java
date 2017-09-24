6c247ef255244b02a4d91c15a53a1e89392e6f78
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-09-24 21:49:11 +0800

    more tests

---
+++ Server/src/test/java/prompto/server/TestResourceInJar.java
@@ -0,0 +1,26 @@
+package prompto.server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import prompto.utils.ManualTests;
+
+@Category(ManualTests.class)
+public class TestResourceInJar {
+	
+			
+	@Test
+	public void testThatResourceCanBeFoundInZipFile() throws IOException {
+		try(ZipFile zip = new ZipFile("/Users/ericvergnaud/.m2/repository/org/prompto/Server/0.0.1-SNAPSHOT/Server-0.0.1-SNAPSHOT.jar")) {
+			ZipEntry entry = zip.getEntry("mimeTypes.json");
+			assertNotNull(entry);
+		}
+	}
+
+}
