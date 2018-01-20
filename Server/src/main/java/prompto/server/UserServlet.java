e56ce6086ebcd76911ab5541cc1868e877f80392
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-01-20 19:11:29 +0800

    add logger and support POST url encoded

--- Server/src/main/java/prompto/server/UserServlet.java
+++ Server/src/main/java/prompto/server/UserServlet.java
@@ -1,6 +1,7 @@
 package prompto.server;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletConfig;
@@ -14,12 +15,13 @@ import prompto.expression.MethodSelector;
 import prompto.grammar.ArgumentAssignmentList;
 import prompto.intrinsic.PromptoDocument;
 import prompto.reader.JSONReader;
-import prompto.runtime.Standalone;
 import prompto.runtime.Context;
 import prompto.runtime.Interpreter;
+import prompto.runtime.Standalone;
 import prompto.statement.MethodCall;
 import prompto.type.DocumentType;
 import prompto.type.TextType;
+import prompto.utils.Logger;
 import prompto.value.Document;
 import prompto.value.ExpressionValue;
 import prompto.value.ListValue;
@@ -28,6 +30,8 @@ import prompto.value.Text;
 
 @SuppressWarnings("serial")
 public class UserServlet extends CleverServlet {
+
+	static final Logger logger = new Logger();
 
 	IMethodDeclaration method;
 	
@@ -44,32 +48,43 @@ public class UserServlet extends CleverServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
+			logger.info(()->"Processing GET " + req.getRequestURI());
 			Context context = Standalone.getGlobalContext();
-			Document document = new Document();
-			req.getParameterMap().forEach((n, l)->{
-				Text name = new Text(n);
-				if(l.length==1)
-					document.setItem(context, name, new Text(l[0]));
-				else {
-					ListValue list = new ListValue(TextType.instance());
-					for(String value : l)
-						list.addItem(new Text(value));
-					document.setItem(context, name, list);
-				}
-			});
-			IExpression args = new ExpressionValue(DocumentType.instance(), document);
-			ArgumentAssignmentList assignments = Interpreter.buildAssignments(method, args);
-			MethodCall call = new MethodCall(new MethodSelector(method.getId()), assignments);
-			call.interpret(context);	
+			Document document = paramsToDocument(context, req.getParameterMap());
+			interpret(context, document);
 		} catch(Throwable t) {
-			t.printStackTrace();
+			logger.error(()->"While processing GET " + req.getRequestURI(), t);
 			resp.setStatus(500);
 		}
 	}
 	
+	private void interpret(Context context, Document document) {
+		IExpression args = new ExpressionValue(DocumentType.instance(), document);
+		ArgumentAssignmentList assignments = Interpreter.buildAssignments(method, args);
+		MethodCall call = new MethodCall(new MethodSelector(method.getId()), assignments);
+		call.interpret(context);	
+	}
+
+	private Document paramsToDocument(Context context, Map<String, String[]> params) {
+		Document document = new Document();
+		params.forEach((n, l)->{
+			Text name = new Text(n);
+			if(l.length==1)
+				document.setItem(context, name, new Text(l[0]));
+			else {
+				ListValue list = new ListValue(TextType.instance());
+				for(String value : l)
+					list.addItem(new Text(value));
+				document.setItem(context, name, list);
+			}
+		});
+		return document;
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
+			logger.info(()->"Processing POST " + req.getRequestURI());
 			String contentType = req.getContentType();
 			if(contentType.startsWith("application/json"))
 				doPostJson(req, resp);
@@ -80,7 +95,7 @@ public class UserServlet extends CleverServlet {
 			else
 				resp.sendError(415);
 		} catch(Throwable t) {
-			t.printStackTrace();
+			logger.error(()->"While processing POST " + req.getRequestURI(), t);
 			resp.setStatus(500);
 		}
 	}
@@ -90,26 +105,19 @@ public class UserServlet extends CleverServlet {
 	}
 
 	private void doPostUrlEncoded(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.sendError(415);
+		Context context = Standalone.getGlobalContext();
+		Document document = paramsToDocument(context, req.getParameterMap());
+		interpret(context, document);
 	}
 
 	private void doPostJson(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		try {
-			Object object = JSONReader.read(req.getInputStream());
-			if(object instanceof PromptoDocument) {
-				Context context = Standalone.getGlobalContext();
-				Document document = new Document(context, (PromptoDocument<?,?>)object);
-				IExpression args = new ExpressionValue(DocumentType.instance(), document);
-				ArgumentAssignmentList assignments = Interpreter.buildAssignments(method, args);
-				MethodCall call = new MethodCall(new MethodSelector(method.getId()), assignments);
-				call.interpret(context);	
-			} else
-				resp.sendError(415);
-		} catch(Throwable t) {
-			t.printStackTrace();
-			resp.setStatus(500);
-		}		
+		Object object = JSONReader.read(req.getInputStream());
+		if(object instanceof PromptoDocument) {
+			Context context = Standalone.getGlobalContext();
+			Document document = new Document(context, (PromptoDocument<?,?>)object);
+			interpret(context, document);
+		} else
+			resp.sendError(415);
 	}
-	
 	
 }
