db95d1a1a377cff48d4154376c694d07a75abbc0
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-01-20 19:14:23 +0800

    read white list from Yaml

--- Server/src/main/java/prompto/config/AuthenticationConfiguration.java
+++ Server/src/main/java/prompto/config/AuthenticationConfiguration.java
@@ -1,5 +1,7 @@
 package prompto.config;
 
+import java.util.Collection;
+
 import prompto.security.BasicAuthenticationMethodFactory;
 
 public class AuthenticationConfiguration extends IAuthenticationConfiguration.Inline {
@@ -10,6 +12,12 @@ public class AuthenticationConfiguration extends IAuthenticationConfiguration.In
 		this.reader = reader;
 		this.authenticationSourceConfiguration = ()->readAuthenticationSourceConfiguration();
 		this.authenticationMethodConfiguration = ()->readAuthenticationMethodConfiguration();
+		this.whiteList = ()->readWhiteList();
+	}
+
+	private Collection<String> readWhiteList() {
+		Collection<String> list = reader.getArray("whiteList");
+		return list!=null ? list : DEFAULT_WHITE_LIST;
 	}
 
 	private IAuthenticationSourceConfiguration readAuthenticationSourceConfiguration() {
