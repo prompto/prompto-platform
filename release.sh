7fa55d4b4864da74869d931147f05193e56cf235
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-09-27 00:07:33 +0800

    one-shot release script

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "version to publish:" version
+cd prompto-java
+mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+mvn clean deploy -P deploy -DskipTests=true
+cd ..
+mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+mvn clean deploy -P deploy -DskipTests=true
+mvn versions:set -DnewVersion=0.0.1-SNAPSHOT -DgenerateBackupPoms=false
+cd prompto-java
+mvn versions:set -DnewVersion=0.0.1-SNAPSHOT -DgenerateBackupPoms=false
