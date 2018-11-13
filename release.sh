bd25226297e72f5285df058c74a02af43f3b9534
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-11-13 23:47:49 +0800

    update release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 read -p "version to publish: " version
+read -p "release name: " name
 cd prompto-java
 mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
 mvn clean deploy -P deploy -DskipTests=true
@@ -9,3 +10,19 @@ mvn clean deploy -P deploy -DskipTests=true
 mvn versions:set -DnewVersion=0.0.1-SNAPSHOT -DgenerateBackupPoms=false
 cd prompto-java
 mvn versions:set -DnewVersion=0.0.1-SNAPSHOT -DgenerateBackupPoms=false
+cd ..
+tag=v$version
+json="{ \"tag_name\": \"$tag\", \"name\": \"$name\" }"
+rm -f release.json
+echo $json >> release.json
+curl --request POST \
+	 --header "Content-Type: application/json" \
+	 --data @release.json \
+	 --user ericvergnaud:$(cat password.txt) \
+	 --url https://api.github.com/repos/prompto/prompto-java/releases
+curl --request POST \
+	 --header "Content-Type: application/json" \
+	 --data @release.json \
+	 --user ericvergnaud:$(cat password.txt) \
+	 --url https://api.github.com/repos/prompto/prompto-platform/releases
+	 
