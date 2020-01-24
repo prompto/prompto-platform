7ec749e61575f31ce60e7f10541da6455768a6ed
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-01-24 08:09:13 +0800

    fix loading stub resources

--- Server/src/main/java/prompto/server/CodeStoreServlet.java
+++ Server/src/main/java/prompto/server/CodeStoreServlet.java
@@ -5,6 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.eclipse.jetty.util.resource.Resource;
 
 import prompto.code.ICodeStore;
+import prompto.code.ResourceReader;
+import prompto.store.AttributeInfo;
+import prompto.store.DataStore;
+import prompto.store.Family;
+import prompto.store.IQueryBuilder;
+import prompto.store.IQueryBuilder.MatchOp;
+import prompto.store.IStore;
+import prompto.store.IStored;
 
 
 @SuppressWarnings("serial")
@@ -16,6 +24,31 @@ public class CodeStoreServlet extends ResourceServlet {
 
 	@Override
 	protected Resource getResource(HttpServletRequest request, String path) {
+		if(path.equals("/stub"))
+			return getStubResource(request, path);
+		else
+			return getCodeResource(request, path);
+	}
+	
+	protected Resource getStubResource(HttpServletRequest request, String path) {
+		try {
+			String moduleId = request.getParameter("moduleId");
+			String resourceName = request.getParameter("resourceName");
+			IStore store = DataStore.getInstance();
+			IQueryBuilder builder = store.newQueryBuilder()
+					.verify(new AttributeInfo("module", Family.CATEGORY, false, null), MatchOp.EQUALS, store.convertToDbId(moduleId))
+					.verify(new AttributeInfo("name", Family.TEXT, false, null), MatchOp.EQUALS, resourceName)
+					.and();
+			IStored stored = store.fetchOne(builder.build());
+			prompto.code.Resource res = ResourceReader.readResource(stored);
+			return res==null ? null : new CodeStoreResource(res);
+		} catch(Throwable t) {
+			t.printStackTrace();
+			return null;
+		}
+	}
+	
+	protected Resource getCodeResource(HttpServletRequest request, String path) {
 		try {
 			if(path.startsWith("/"))
 				path = path.substring(1);
