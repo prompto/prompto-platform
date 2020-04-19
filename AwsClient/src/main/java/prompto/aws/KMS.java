afcb4563644287946dd4ef76bd0ead3a2441fc77
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-04-19 17:51:22 +0800

    Gracefully cleanup invalid access keys

--- AwsClient/src/main/java/prompto/aws/KMS.java
+++ AwsClient/src/main/java/prompto/aws/KMS.java
@@ -3,6 +3,9 @@ package prompto.aws;
 import java.nio.ByteBuffer;
 import java.util.Base64;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -21,16 +24,38 @@ import com.amazonaws.services.kms.model.ListAliasesResult;
 
 public class KMS {
 	
+	static Logger logger = LoggerFactory.getLogger(KMS.class);
+	
 	public static KMS newInstance(String awsRegion, String login, String password) {
 		AWSKMSClientBuilder builder = AWSKMSClientBuilder.standard()
 				.withRegion(awsRegion);
+		login = verifyLogin(login);
 		if(login!=null && password!=null) {
+			log("Connecting to " + awsRegion + " with login "  + login);
 			AWSCredentials credentials = new BasicAWSCredentials(login, password);
 			builder = builder.withCredentials(new AWSStaticCredentialsProvider(credentials));
+		} else {
+			log("Connecting to " + awsRegion + " through IAM");
 		}
 		return new KMS(builder.build());
 	}
 	
+	private static String verifyLogin(String login) {
+		if(login!=null) {
+			if(login.trim().isEmpty() || "null".equals(login)) {
+				log("Invalid login: " + login);
+				return null;
+			}
+		}
+		return login;
+	}
+	
+	private static void log(String msg) {
+		logger.info(msg);
+		// logger may not be connected yet, so also copy to err
+		System.err.println(msg);
+	}
+
 	AWSKMS kms;
 
 	public KMS(AWSKMS kms) {
@@ -65,17 +90,22 @@ public class KMS {
 	}
 	
 	public String keyARNFromAlias(String alias) {
+		logger.info("Fetching ARN for " + alias);
 		final String fullAlias = "alias/" + alias;
 		ListAliasesResult res = kms.listAliases();
 		AliasListEntry entry = res.getAliases().stream()
 				.filter(a->fullAlias.equals(a.getAliasName()))
 				.findFirst()
 				.orElse(null);
-		if(entry==null)
+		if(entry==null) {
+			log("No ARN found for " + alias);
 			return null;
+		}
 		String prefix = entry.getAliasArn();
 		prefix = prefix.substring(0, prefix.indexOf(":" + fullAlias));
-		return prefix + ":key/" + entry.getTargetKeyId(); 
+		String result = prefix + ":key/" + entry.getTargetKeyId(); 
+		log("Found ARN: " + result);
+		return result;
 	}
 	
 	public String encrypt(String masterKeyARN, String dataToEncrypt) {
@@ -85,10 +115,13 @@ public class KMS {
 			.withPlaintext(bytes);
 		EncryptResult res = kms.encrypt(req);
 		bytes = res.getCiphertextBlob();
-		return Base64.getEncoder().encodeToString(bytes.array());
+		String result = Base64.getEncoder().encodeToString(bytes.array());
+		log("Encrypted " + result);
+		return result;
 	}
 	
 	public String decrypt(String dataToDecrypt) {
+		log("Decrypting " + dataToDecrypt);
 		ByteBuffer bytes = ByteBuffer.wrap(Base64.getDecoder().decode(dataToDecrypt));
 		DecryptRequest req = new DecryptRequest()
 			.withCiphertextBlob(bytes);
