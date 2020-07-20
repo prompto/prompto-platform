aea31f6622a761619ad7ec40d559c1e4e024655a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-07-20 22:59:51 +0800

    support input parameters - scalar

--- Server/src/main/java/prompto/graphql/GraphQLMethodFetcher.java
+++ Server/src/main/java/prompto/graphql/GraphQLMethodFetcher.java
@@ -3,7 +3,13 @@ package prompto.graphql;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import prompto.declaration.IMethodDeclaration;
+import prompto.java.JavaClassType;
+import prompto.param.IParameter;
 import prompto.runtime.ApplicationContext;
+import prompto.runtime.Context;
+import prompto.type.IType;
+import prompto.value.IValue;
+import prompto.value.NullValue;
 
 public class GraphQLMethodFetcher implements DataFetcher<Object> {
 
@@ -15,7 +21,23 @@ public class GraphQLMethodFetcher implements DataFetcher<Object> {
 
 	@Override
 	public Object get(DataFetchingEnvironment environment) throws Exception {
-		return method.interpret(ApplicationContext.get());
+		Context context = ApplicationContext.get().newLocalContext();
+		method.getParameters().forEach(param->populateArgument(context, param, environment));
+		return method.interpret(context);
+	}
+
+	private void populateArgument(Context context, IParameter param, DataFetchingEnvironment environment) {
+		context.registerValue(param);
+		Object data = environment.getArgument(param.getName());
+		IValue value = convert(context, data, param.getType(context));
+		context.setValue(param.getId(), value);
+	}
+
+	private IValue convert(Context context, Object data, IType type) {
+		if(data==null)
+			return NullValue.instance();
+		else
+			return JavaClassType.convertJavaValueToPromptoValue(context, data, data.getClass(), type);
 	}
 
 }
