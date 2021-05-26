a1ad494f3659d86a8639fafe6f1cc7fa8ffb1ee8
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-05-26 12:48:17 +0200

    fix script

--- build_and_publish_artifacts.sh
+++ build_and_publish_artifacts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
 mvn clean deploy -P deploy -DskipTests=true
 deploy=$?
 mvn versions:set -DnewVersion=0.0.1-SNAPSHOT -DgenerateBackupPoms=false
