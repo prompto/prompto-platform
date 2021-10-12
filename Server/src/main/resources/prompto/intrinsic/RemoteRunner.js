c2116fa020c81711bba966b58425739e459778b7
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-10-12 10:58:52 +0200

    support synchronous remote calls (useful for testing)

--- Server/src/main/resources/prompto/intrinsic/RemoteRunner.js
+++ Server/src/main/resources/prompto/intrinsic/RemoteRunner.js
@@ -1,6 +1,18 @@
 var RemoteRunner = {
 		
 	run: function(name, params, andThen, bindTo) {
+			var formData = this.makeParams(params);
+		    axios.post('/ws/run/' + name, formData).then(function(response) {
+		    	 response = response.data
+		    	 if(response.error) 
+		    		 alert(response.error); // TODO throw ?
+		    	 else {	 
+		     		var result = readJSONValue(response.data);
+		     		andThen.bind(bindTo)(result);
+		    	 }
+	     	});
+		},
+	makeParams: function(params) {
 		var formData = new FormData();
 		params.forEach(function(param) {
 			var value = writeJSONValue(param.value, false, formData);
@@ -11,16 +23,21 @@ var RemoteRunner = {
 				param.value = value;
 		});
 		formData.append("params", JSON.stringify(params));
-	    axios.post('/ws/run/' + name, formData).then(function(response) {
-	    	 response = response.data
-	    	 if(response.error) 
-	    		 alert(response.error); // TODO throw ?
-	    	 else {	 
-	     		var result = readJSONValue(response.data);
-	     		andThen.bind(bindTo)(result);
-	    	 }
-     	});
-	     // TODO catch
-	}
+		return formData;
+		},
+	runSync: function(name, params) {	
+			var formData = this.makeParams(params);
+			var response = null;
+			var request  = new XMLHttpRequest();
+			request.open("POST", "/ws/run/" + name, false); // make call synchronous
+			request.onload = function() { 
+				if (this.status == 200)
+					response = JSON.parse(this.responseText); 
+				else
+					throw new Error(this.statusText);
+			};
+			request.send(formData);
+			return response && response.data ? readJSONValue(response.data) : null;
+		}
 
 };
