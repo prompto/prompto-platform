849f161707183df4f3f09e727e41e1785e2674d5
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-02-10 23:51:46 +0100

    use jdk 11

--- build_and_publish_artifacts.sh
+++ build_and_publish_artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_271.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home
 mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
-mvn clean deploy -P deploy -DskipTests=true
+mvn -e clean deploy -P deploy -DskipTests=true
 deploy=$?
 mvn versions:set -DnewVersion=0.0.1-SNAPSHOT -DgenerateBackupPoms=false
 exit $deploy
